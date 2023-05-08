5a3f9c42b73f86085037318387d5f7ec488b0c0c
Pavan C <pavanreddy0530@gmail.com>
2023-05-08 13:51:25 +0530

    Adding all script.

--- catalogue.sh
+++ catalogue.sh
@@ -4,12 +4,12 @@ useradd roboshop
 mkdir /app
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-unzip /tmp/user.zip
+unzip /tmp/catalogue.zip
 npm install
-cp user.service /etc/systemd/system/user.service
+cp catalogue.service /etc/systemd/system/user.service
 systemctl daemon-reload
-systemctl enable user
-systemctl start user
+systemctl enable catalogue
+systemctl start catalogue 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.pdevopsb72.online </app/schema/catalogue.js
