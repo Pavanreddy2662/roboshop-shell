6eec982655c01a0fe1306274e3055af6a4a337f6
Pavan C <pavanreddy0530@gmail.com>
2023-05-09 15:25:14 +0530

    Adding paths

--- user.sh
+++ user.sh
@@ -1,15 +1,39 @@
+echo -e "\e[36m>>>>>>>>> configuring NodeJS repos <<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<<<\e[0m"
 useradd roboshop
+
+echo -e "\e[36m>>>>>>>>> Create Application Directory <<<<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>> Download App Content <<<<<<<<<<\e[0m"
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+
+echo -e "\e[36m>>>>>>>>> Unzip App Content <<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/user.zip
+
+echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<<<\e[0m"
 npm install
-cp user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>>>>> Copy Catalogue SystemD <<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+
+echo -e "\e[36m>>>>>>>>> Start Catalogue Service <<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl start user
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>> Copy MongoDB Repo <<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>> Install MongoDB Client <<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
-mongo --host mongodb-dev.pdevopsb72.online </app/schema/catalogue.js
+
+echo -e "\e[36m>>>>>>>>> Load schema <<<<<<<<<<\e[0m"
+mongo --host mongodb-dev.pdevopsb72.online </app/schema/user.js
