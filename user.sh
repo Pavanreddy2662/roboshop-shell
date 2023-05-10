ef336a7dc0520f1cdf9e6adbcd0ee299170fd4c9
Pavan C <pavanreddy0530@gmail.com>
2023-05-10 07:42:04 +0530

    User services

--- user.sh
+++ user.sh
@@ -1,7 +1,7 @@
 echo -e "\e[36m>>>>>>>>> configuring NodeJS repos <<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo -e "\e[36m>>>>>>>>> Install NodeJS <<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> install NodeJS <<<<<<<<<<\e[0m"
 yum install nodejs -y
 
 echo -e "\e[36m>>>>>>>>> Add Application User <<<<<<<<<<\e[0m"
@@ -21,10 +21,11 @@ unzip /tmp/user.zip
 echo -e "\e[36m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<<<\e[0m"
 npm install
 
-echo -e "\e[36m>>>>>>>>> create Application Directory <<<<<<<<<<\e[0m"
+
+echo -e "\e[36m>>>>>>>>> Copy user SystemD <<<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
 
-echo -e "\e[36m>>>>>>>>> Start User Service <<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Start user Service <<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
